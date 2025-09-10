80c01df844c64d1789ee8cd02bb5fc59b6c16e9b
akgupta766 <akgupta7606@gmail.com>
2025-09-10 21:18:57 +0530

    Add files via upload

---
+++ Frontend/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Nexathon_1\\project" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "PROJECT_DIR=D:\\Nexathon_1\\project"
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Nexathon_1\\project\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Nexathon_1\\project"
+  "FLUTTER_TARGET=D:\\Nexathon_1\\project\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzcuMC0xLjAucHJlLTYw,RkxVVFRFUl9DSEFOTkVMPW1hc3Rlcg==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YTA4MjA5NjRkNQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZWNiOWVjMmQ5ZA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC4wIChidWlsZCAzLjEwLjAtMTg0LjAuZGV2KQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Nexathon_1\\project\\.dart_tool\\package_config.json"
+)
